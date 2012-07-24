4f29cb57b544c70817088b50c09f644e14f6fe77
Terin Stock <terinjokes@gmail.com>
2012-07-24 16:49:18 -0400

    Reply to message using smtplib.

--- autoresponder.py
+++ autoresponder.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
 import imaplib
 import smtplib
 import email
+import time
+from email.mime.text import MIMEText
 
 IMAP_SERVER = "imap.gmail.com"
 SMTP_SERVER = "smtp.gmail.com"
 USERNAME = "user@domain.tld"
 PASSWORD = "secret"
+FROM_HEADER = "Auto Responder User <user@domain.tld>"
 
 
-def makeConnection(server, username, password):
+def makeIMAPConnection(server, username, password):
     imapServer = imaplib.IMAP4_SSL(server)
     imapServer.login(username, password)
     imapServer.select('INBOX')
@@ -33,10 +38,29 @@ def genEmailForUIDs(uids, imapServer):
         raw_email = data[0][1]
         emailObj = email.message_from_string(raw_email)
         yield emailObj
+        #handle moving out of inbox here
+
+def makeSMTPConnection(server, username, password):
+    smtpServer = smtplib.SMTP_SSL(server)
+    smtpServer.login(username, password)
+    return smtpServer
 
 
-imapServer = makeConnection(SERVER, USERNAME, PASSWORD)
+imapServer = makeIMAPConnection(IMAP_SERVER, USERNAME, PASSWORD)
 if hasNewMail(imapServer):
+    smtpServer = makeSMTPConnection(SMTP_SERVER, USERNAME, PASSWORD)
     uids = UIDsForNewEmail(imapServer)
     for emailObj in genEmailForUIDs(uids, imapServer):
-        print emailObj['To']
+        replyObj = MIMEText("Test Unicode Reply: ᛁᚳ᛫ᛗᚨᚷ᛫ᚷᛚᚨᛋ᛫ᛖᚩᛏᚪᚾ᛫ᚩᚾᛞ᛫ᚻᛁᛏ᛫ᚾᛖ᛫ᚻᛖᚪᚱᛗᛁᚪᚧ᛫ᛗᛖ᛬", "plain", "utf-8")
+        # TODO (Terin Stock): Do I need to ensure these are clean, or does
+        # email handle that?
+        replyObj['To'] = emailObj['From'] if emailObj['Reply-To'] != "None" else emailObj['Reply-To']
+        replyObj['From'] = FROM_HEADER
+        replyObj['Date'] = email.utils.formatdate(time.time(), True)
+        replyObj['References'] = emailObj['Message-ID'] #According to jwz, RFC1036 allows us to truncate references as we see fit
+        replyObj['In-Reply-To'] = emailObj['Message-ID']
+        replyObj['Subject'] = emailObj['Subject']
+        smtpServer.sendmail(FROM_HEADER, replyObj['To'], replyObj.as_string())
+    smtpServer.quit()
+imapServer.close()
+imapServer.logout()
