900385524a338309d7821cef8b641dcdaee0766d
Terin Stock <terinjokes@gmail.com>
2012-07-25 17:56:45 -0400

    Removing generators. Making it all "simple and boring"

--- autoresponder.py
+++ autoresponder.py
@@ -9,12 +9,38 @@ import yaml
 import argparse
 from email.mime.text import MIMEText
 
+# pragma mark - Server Connections
 
-def makeIMAPConnection(server, port, ssl, username, password):
-    imapServer = imaplib.IMAP4_SSL(server)
+
+def makeIMAPServer(server, port, ssl, username, password):
+    imapServer = imaplib.IMAP4_SSL(server, port) if ssl else \
+            imaplib.IMAP(server, port)
     imapServer.login(username, password)
     imapServer.select('INBOX')
     return imapServer
+
+
+def makeSMTPServer(server, port, ssl, username, password):
+    smtpServer = smtplib.SMTP_SSL(server, port) if ssl else \
+            smtplib.SMTP(server, port)
+    smtpServer.login(username, password)
+    return smtpServer
+
+
+def makeIMAPServerWithConfig(imapConfig):
+    imapServer = makeIMAPServer(imapConfig["server"],
+            imapConfig["port"], imapConfig["ssl"], imapConfig["username"],
+            imapConfig["password"])
+    return imapServer
+
+
+def makeSMTPServerWithConfig(smtpConfig):
+    smtpServer = makeSMTPServer(smtpConfig["server"],
+            smtpConfig["port"], smtpConfig["ssl"], smtpConfig["username"],
+            smtpConfig["password"])
+    return smtpServer
+
+# pragma mark - IMAP Utils
 
 
 def hasNewMail(imapServer):
@@ -29,35 +55,31 @@ def UIDsForNewEmail(imapServer):
     return uids
 
 
-def genEmailForUIDs(uids, imapServer):
-    for uid in uids:
-        _, data = imapServer.uid('fetch', uid, '(RFC822)')
-        raw_email = data[0][1]
-        emailObj = email.message_from_string(raw_email)
-        yield emailObj
-        #handle moving out of inbox here
+def emailForUID(uid, imapServer):
+    _, data = imapServer.uid('fetch', uid, '(RFC822)')
+    raw_email = data[0][1]
+    emailObj = email.message_from_string(raw_email)
+    return emailObj
 
 
-def makeSMTPConnection(server, port, ssl, username, password):
-    smtpServer = smtplib.SMTP_SSL(server)
-    smtpServer.login(username, password)
-    return smtpServer
+#pragma mark -
 
 
-def genForConfig(config):
-    for server in config:
-        imapConfig = server["imap"]
-        smtpConfig = server["smtp"]
-        imapServer = makeIMAPConnection(imapConfig["server"],
-                imapConfig["port"], imapConfig["ssl"], imapConfig["username"],
-                imapConfig["password"])
-        smtpServer = makeSMTPConnection(smtpConfig["server"],
-                smtpConfig["port"], smtpConfig["ssl"], smtpConfig["username"],
-                smtpConfig["password"])
-        yield (imapServer, smtpServer, smtpConfig["from"], server["body"])
-        imapServer.close()
-        imapServer.logout()
-        smtpServer.quit()
+def replyWithOriginalEmail(emailObj, fromAddress, body):
+    replyObj = MIMEText(body, _charset="utf-8")
+    # TODO (Terin Stock): Do I need to ensure these are clean,
+    # or does 'email' handle that?
+    replyObj['To'] = emailObj['From']
+    if emailObj['Reply-To'] != "None":
+        replyObj['To'] = emailObj['Reply-To']
+    replyObj['From'] = fromAddress
+    replyObj['Date'] = email.utils.formatdate(time.time(), True)
+    # According to jwz, RFC1036 allows us to truncate references
+    # (http://www.jwz.org/doc/threading.html)
+    replyObj['References'] = emailObj['Message-ID']
+    replyObj['In-Reply-To'] = emailObj['Message-ID']
+    replyObj['Subject'] = emailObj['Subject']
+    return replyObj
 
 
 def parseArgs():
@@ -69,27 +91,23 @@ def parseArgs():
 def main():
     args = parseArgs()
     config = yaml.load(args.config)
-    for (imapServer, smtpServer, smtpFrom, emailBody) in genForConfig(config):
+    args.config.close()
+    for server in config:
+        imapServer = makeIMAPServerWithConfig(server['imap'])
         if hasNewMail(imapServer):
+            smtpServer = makeSMTPServerWithConfig(server['smtp'])
             uids = UIDsForNewEmail(imapServer)
-            for emailObj in genEmailForUIDs(uids, imapServer):
-                replyObj = MIMEText(emailBody, _charset="utf-8")
-                # TODO (Terin Stock): Do I need to ensure these are clean,
-                # or does 'email' handle that?
-                replyObj['To'] = emailObj['From']
-                if emailObj['Reply-To'] != "None":
-                    replyObj['To'] = emailObj['Reply-To']
-                replyObj['From'] = smtpFrom
-                replyObj['Date'] = email.utils.formatdate(time.time(), True)
-                # According to jwz, RFC1036 allows us to truncate references
-                # (http://www.jwz.org/doc/threading.html)
-                replyObj['References'] = emailObj['Message-ID']
-                replyObj['In-Reply-To'] = emailObj['Message-ID']
-                replyObj['Subject'] = emailObj['Subject']
-                smtpServer.sendmail(smtpFrom,
+            for uid in uids:
+                emailObj = emailForUID(uid, imapServer)
+                replyObj = replyWithOriginalEmail(emailObj,
+                        server['smtp']['from'],
+                        server['body'])
+                smtpServer.sendmail(server['smtp']['from'],
                         replyObj['To'],
                         replyObj.as_string())
-    args.config.close()
+            smtpServer.quit()
+        imapServer.close()
+        imapServer.logout()
 
 
 if __name__ == "__main__":
