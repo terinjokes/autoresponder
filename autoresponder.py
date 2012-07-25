24ae3590b74ad986299975d20189c453f5b6b1a7
Terin Stock <terinjokes@gmail.com>
2012-07-25 14:47:17 -0400

    Making `flake8` happy

--- autoresponder.py
+++ autoresponder.py
@@ -7,17 +7,14 @@ import time
 import sys
 import yaml
 from email.mime.text import MIMEText
-from yaml import load
-try:
-    from yaml import CLoader as Loader
-except ImportError:
-    from yaml import Loader
+
 
 def makeIMAPConnection(server, port, ssl, username, password):
     imapServer = imaplib.IMAP4_SSL(server)
     imapServer.login(username, password)
     imapServer.select('INBOX')
     return imapServer
+
 
 def hasNewMail(imapServer):
     status, data = imapServer.status('INBOX', '(UNSEEN)')
@@ -27,10 +24,12 @@ def hasNewMail(imapServer):
     else:
         return False
 
+
 def UIDsForNewEmail(imapServer):
     status, data = imapServer.uid('search', None, '(UNSEEN)')
     uids = data[0].split()
     return uids
+
 
 def genEmailForUIDs(uids, imapServer):
     for uid in uids:
@@ -40,10 +39,12 @@ def genEmailForUIDs(uids, imapServer):
         yield emailObj
         #handle moving out of inbox here
 
+
 def makeSMTPConnection(server, port, ssl, username, password):
     smtpServer = smtplib.SMTP_SSL(server)
     smtpServer.login(username, password)
     return smtpServer
+
 
 def genForConfig(config):
     for server in config:
@@ -51,14 +52,15 @@ def genForConfig(config):
         smtpConfig = server["smtp"]
         imapServer = makeIMAPConnection(imapConfig["server"],
                 imapConfig["port"], imapConfig["ssl"], imapConfig["username"],
-                imapConfig["password"]);
+                imapConfig["password"])
         smtpServer = makeSMTPConnection(smtpConfig["server"],
                 smtpConfig["port"], smtpConfig["ssl"], smtpConfig["username"],
-                smtpConfig["password"]);
+                smtpConfig["password"])
         yield (imapServer, smtpServer, smtpConfig["from"], server["body"])
         imapServer.close()
         imapServer.logout()
         smtpServer.quit()
+
 
 def main(args):
     fileObj = open(args[1], 'r')
@@ -68,17 +70,24 @@ def main(args):
             uids = UIDsForNewEmail(imapServer)
             for emailObj in genEmailForUIDs(uids, imapServer):
                 replyObj = MIMEText(emailBody, _charset="utf-8")
-                # TODO (Terin Stock): Do I need to ensure these are clean, or does
-                # email handle that?
-                replyObj['To'] = emailObj['From'] if emailObj['Reply-To'] != "None" else emailObj['Reply-To']
+                # TODO (Terin Stock): Do I need to ensure these are clean,
+                # or does 'email' handle that?
+                replyObj['To'] = emailObj['From']
+                if emailObj['Reply-To'] != "None":
+                    replyObj['To'] = emailObj['Reply-To']
                 replyObj['From'] = smtpFrom
                 replyObj['Date'] = email.utils.formatdate(time.time(), True)
-                replyObj['References'] = emailObj['Message-ID'] #According to jwz, RFC1036 allows us to truncate references as we see fit
+                # According to jwz, RFC1036 allows us to truncate references
+                # (http://www.jwz.org/doc/threading.html)
+                replyObj['References'] = emailObj['Message-ID']
                 replyObj['In-Reply-To'] = emailObj['Message-ID']
                 replyObj['Subject'] = emailObj['Subject']
-                smtpServer.sendmail(FROM_HEADER, replyObj['To'], replyObj.as_string())
+                smtpServer.sendmail(smtpFrom,
+                        replyObj['To'],
+                        replyObj.as_string())
     fileObj.close()
     return None
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
